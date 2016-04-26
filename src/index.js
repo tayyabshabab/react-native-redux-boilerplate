826850d795edd6404d14e5f5be5334d0ecc0f210
Tayyab Shabab <tayyabshabab@users.noreply.github.com>
2016-04-26 13:50:20 +0200

    Adds support for redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
 import App from './containers/App';
+
+const store = configureStore();
 
 export default class root extends Component {
   render() {
     return (
+      <Provider store={store}>
         <App />
+      </Provider>
     );
   }
 }
