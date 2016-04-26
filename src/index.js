db3e069478bc2c3e852f1b02f98d64c5235eb3ba
Tayyab Shabab <tayyabshabab@users.noreply.github.com>
2016-04-26 11:50:32 +0200

    React native Initialized

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react-native';
+
+import App from './containers/App';
+
+export default class root extends Component {
+  render() {
+    return (
+        <App />
+    );
+  }
+}
